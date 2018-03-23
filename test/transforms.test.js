9ef0af9ad4cb1667c72910189e19d4d67ec79835
David Nunez <arizonatribe@gmail.com>
2018-03-22 17:38:53 -0700

    Improved the hashEm() function to exclude items that didn't have an id prop and also to make the function point-free (in this case, that might have made it less readable; sorry about that)

--- test/transforms.test.js
+++ test/transforms.test.js
@@ -1,5 +1,5 @@
 import test from 'tape'
-import {dateMe, toUSD} from '../lib/transforms'
+import {hashEm, dateMe, toUSD} from '../lib/transforms'
 
 test('pretty formatting of date includes time and shaves off unnecessary zero-padding', (t) => {
     t.equal(dateMe('01/01/2018'), '1/1/2018, 12:00:00 AM')
@@ -17,5 +17,27 @@ test('transform to U.S. dollars is always prefixed with the dollar-sign characte
     t.equal(toUSD(0), '$0.00', 'no money')
     t.equal(toUSD(15333), '$15,333.00', 'includes comma-separation')
     t.equal(toUSD(20.18), '$20.18', 'includes values to the right of decimal')
+    t.end()
+})
+
+test('A collection of objects is hash-mapped with a key of "id" and a value of index', (t) => {
+    t.deepEqual(
+        hashEm([{id: 'lorem'}, {id: 'ipsum'}, {id: 'dolor'}, {id: 'sit'}]),
+        {lorem: 0, ipsum: 1, dolor: 2, sit: 3}
+    )
+    t.end()
+})
+
+test('A collection of objects is NOT hash-mapped if there is no "id" prop in the collection', (t) => {
+    t.deepEqual(
+        hashEm([{name: 'tacitus'}, {name: 'seneca'}, {name: 'virgil'}, {name: 'aurelius'}]),
+        {},
+        'when no ids are present, the hashmap will be empty'
+    )
+    t.deepEqual(
+        hashEm([{id: 'lorem'}, {name: 'seneca'}, {id: 'dolor'}, {id: 'sit'}]),
+        {lorem: 0, dolor: 2, sit: 3},
+        'when some objects are missing an id, they are excluded from the hashmap'
+    )
     t.end()
 })
